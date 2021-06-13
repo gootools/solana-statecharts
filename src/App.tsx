f2809e8f92bd1bbb63c92e6afeb9358d894f4553
John Rees <john@bitsushi.com>
2021-06-13 15:19:18 +0100

    start adding dynamic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Form from "@rjsf/core";
 import { useMachine } from "@xstate/react";
 import React from "react";
 import { Machine } from "xstate";
 import cashiersCheck from "./machines/cashiersCheck";
+
+const schema = {
+  title: "createCheck",
+  type: "object",
+  required: ["amount"],
+  properties: {
+    amount: { type: "number" },
+    memo: { type: "string" },
+    nonce: { type: "number" },
+  },
+} as any;
+
+const log = (type: string) => console.log.bind(console, type);
 
 const machine = Machine(cashiersCheck as any);
 
@@ -10,6 +24,13 @@ const App: React.FC = () => {
   return (
     <>
       <h1>{state.value}</h1>
+      <Form
+        schema={schema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      />
+
       {machine.states[state.value as any].events.map((event) => (
         <button key={event} onClick={() => send(event)}>
           {event}
